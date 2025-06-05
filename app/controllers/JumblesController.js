0c1a0b2d30778a159fa30d262c6f6cca847f3336
Anthony Rossman <anthony.rossman0825@gmail.com>
2025-06-05 16:54:50 -0600

    save load works now, yay

--- app/controllers/JumblesController.js
+++ app/controllers/JumblesController.js
@@ -1,6 +1,7 @@
 import { AppState } from "../AppState.js";
 import { Jumble } from "../models/Jumble.js";
 import { jumblesService } from "../services/JumblesService.js";
+import { getFormData } from "../utils/FormHandler.js";
 import { setHTML } from "../utils/Writer.js";
 
 
@@ -9,13 +10,18 @@ export class JumblesController {
     console.log('🎉');
     this.drawJumbleList()
     AppState.on('activeJumble', this.drawActiveJumble);
+    AppState.on('jumbles', this.drawJumbleList);
+    AppState.on('jumbles', jumblesService.saveJumbles);
+    jumblesService.loadJumbles();
   }
 
   drawJumbleList(){
+
     const jumbleCards = AppState.jumbles
     let listContent = ""
     jumbleCards.forEach(card => listContent += card.jumbleListCardHTMLTemplate)
     setHTML('jumble-list', listContent)
+    console.log('drawlist')
   }
 
   setActiveJumble(jumbleId) {
@@ -27,5 +33,16 @@ export class JumblesController {
   drawActiveJumble() {
     const setActiveJumble = AppState.activeJumble;
     setHTML('active-card', setActiveJumble.activeJumbleTemplate)
+  }
+
+  getNewJumbleData(){
+    event.preventDefault();
+    const form = document.getElementById("new-jumble")
+    const formData = getFormData(form)
+    
+    jumblesService.createNewJumble(formData);
+
+
+    console.log('getNewData controller')
   }
 }
--- app/services/JumblesService.js
+++ app/services/JumblesService.js
@@ -1,9 +1,11 @@
 import { AppState } from "../AppState.js";
+import { Jumble } from "../models/Jumble.js";
+import { loadState, saveState } from "../utils/Store.js";
 
 
 class JumblesService {
 
-  
+
 
   activeJumble(jumbleId) {
 
@@ -12,6 +14,28 @@ class JumblesService {
     console.log("set dat ish");
   }
 
+  createNewJumble(formData){
+    const newJumble = new Jumble(formData)
+    AppState.jumbles.push(newJumble)
+
+    console.log('createnew');
+
+  }
+
+    saveJumbles() {
+      saveState('jumbles', AppState.jumbles);
+      console.log('saving this ish')
+
+    }
+
+    loadJumbles() {
+      const loadedJumbles = loadState('jumbles', [Jumble]);
+      if (loadedJumbles.length > 0) {
+      AppState.jumbles = loadedJumbles;
+      } else {
+        console.log('no jumbles');
+      }
+    }
 }
 
 export const jumblesService = new JumblesService();
