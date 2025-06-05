35e6166f2dc8c78c26505c55d3acb323b2811365
BlueFyre22 <bluefyr22@gmail.com>
2025-06-05 14:18:42 -0600

    starting the project

---
+++ app/App.js
@@ -0,0 +1,11 @@
+import { ExampleController } from './controllers/ExampleController.js';
+
+class App {
+
+
+
+}
+
+window['app'] = new App()
+
+
---
+++ app/models/Jumble.js
@@ -0,0 +1,20 @@
+import { generateId } from "../utils/GenerateId.js";
+
+
+export class Jumble {
+  constructor(data) {
+    this.id = generateId();
+    this.name = data.name;
+    this.body = data.body;
+
+    //worry about these later
+    this.fastestTime = null;
+    this.startTime = null;
+    this.endTime = null;
+  }
+
+  get ListTemplate() {
+    return `
+    <li>${this.name}</li>`;
+  }
+}
